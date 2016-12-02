509deb31887efea80abbee12a96b87e60deaec6c
Andy Herzog <andyherzog@Andys-MacBook-Pro.local>
2016-12-02 14:46:14 -0700

    working

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const cors = require('cors')
+const cors = require('cors');
 
 app.use(cors());
 
@@ -26,5 +26,4 @@ const cerial = [{
 
 app.get('/cerial', function(req, res, next){
   res.send(cerial);
-  console.log('hi');
 });
